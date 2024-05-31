6132b497107b1cc2b3971209c3616b88700e74bf
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-05-31 14:42:44 +0200

    [TRELLO-2344] Send push notif based on categories

--- src/clients/onesignal.client.ts
+++ src/clients/onesignal.client.ts
@@ -1,25 +1,24 @@
 import {Config} from '../config/config.js'
 
-export const pushNotif = async (subCategories: string[], count: number) => {
-  const filters = subCategories
-    .flatMap(category => {
-      return [{field: 'tag', key: category, relation: 'exists'}, {operator: 'OR'}]
+export const pushNotif = async (tags: string[], count: number) => {
+  const filters = tags
+    .flatMap(tag => {
+      return [{field: 'tag', key: tag, relation: 'exists'}, {operator: 'OR'}]
     })
     .slice(0, -1)
 
   console.log('Generated one signal filters', filters)
 
   const frContent =
-    count > 1 ? `${count} nouveaux produits rappelés` : `Un nouveau produit a été rappelé dans la catégorie ${subCategories[0]}`
-  const enContent = ""
-
+    count > 1 ? `${count} nouveaux produits rappelés` : `Un nouveau produit a été rappelé dans la catégorie ${tags[0]}`
+  const enContent = count > 1 ? `${count} new products recalled` : `One new product has been recalled in ${tags[0]}`
 
   const fetchOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': `Basic ${Config.oneSignaleApiKey}`,
+      Accept: 'application/json',
+      Authorization: `Basic ${Config.oneSignaleApiKey}`,
     },
     body: JSON.stringify({
       app_id: Config.oneSignalAppId,
